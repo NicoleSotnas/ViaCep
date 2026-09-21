33ef443319e34994c9d0c49079798a750bb9392c
Nicole Santos <gitnicole.santos@gmail.com>
2026-09-21 10:49:50 -0300

    Tratamento de Falhas e Mensagens

--- src/app/app.ts
+++ src/app/app.ts
@@ -23,18 +23,27 @@ export class App {
     this.erro.set('');
     this.endereco.set(null);
     const cep = this.cepControl.value!;
+
+    // Faz a requisição para a API de CEP
     this.cepService.buscarCep(cep).subscribe({
+      //next -> a resposta da API foi recebida com sucesso
       next: (resposta) => {
-        this.loading.set(false);
+        this.loading.set(false); //Parar de exibir a mensagem de aguardando resposta do servidor
+
+        //Verifica se a API retornou um erro de usuário que digitou um cep inexistente (CEP não encontrado)
         if (resposta.erro) {
           this.erro.set('CEP não encontrado.');
           return;
         }
+
+        // Exibe o endereço completo retornado pela API (cep existente)
         this.endereco.set(resposta);
       },
+      // error -> ocorreu algum erro na requisição
       error: () => {
-        this.loading.set(false);
-        this.erro.set('Erro ao consultar API.');
+        this.loading.set(false);//Parar de exibir a mensagem de aguardando resposta do servidor
+        
+        this.erro.set('Erro ao consultar API.'); // =Exibe uma mensagem de erro para o usuário
       },
     });
   }
--- src/app/services/cep.ts
+++ src/app/services/cep.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Cep } from '../models/cep';
 @Injectable({
@@ -10,6 +11,6 @@ import { Cep } from '../models/cep';
 export class CepService {
   private http = inject(HttpClient);
   buscarCep(cep: string) {
-    return this.http.get<Cep>(`https://viacep.com.br/ws/${cep}/json/`);
+    return this.http.get<Cep>(`https://viacep.com.br/ws/${cep}/json/`).pipe(retry(2));
   }
 }
